da225fb41aba23e5a21313334ab96e995176dae3
addoun abdellah <addoun.abdellah@gmail.com>
2019-12-09 16:46:09 +0100

    Creating sign in form

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -8,7 +8,8 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
-
+  String email = '';
+  String password = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,18 +19,47 @@ class _SignInState extends State<SignIn> {
         elevation: 0.0,
         title: Text('Sign In to Brew Crew'),
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 15.0),
-        child: RaisedButton(
-          child: Text('Sign In Anonymously'),
-          onPressed: () async {
-            dynamic result = await _auth.signInAnonymously();
-            if (result == null) {
-              print('damn, something happedn !!');
-            } else {
-              print('signed in, uid = ${result.uid}');
-            }
-          },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                obscureText: true,
+                onChanged: (value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              RaisedButton(
+                color: Colors.brown[700],
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {},
+              )
+            ],
+          ),
         ),
       ),
     );
