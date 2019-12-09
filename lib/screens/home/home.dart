114757e8cce84e6625e6f27cc8710331b603ee75
addoun abdellah <addoun.abdellah@gmail.com>
2019-12-09 16:06:09 +0100

    Implementing log out feature

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,26 @@
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  final AuthService auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home'),
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text('Brew Crew'),
+        backgroundColor: Colors.brown[200],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('log out'),
+            onPressed: () async {
+              await auth.signOut();
+            },
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,13 @@
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:brew_crew/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/models/user.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Authenticate();
+    final user = Provider.of<User>(context);
+    return user == null ? Authenticate() : Home();
   }
 }
