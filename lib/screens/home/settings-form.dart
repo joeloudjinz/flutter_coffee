eca1df4b2586f3387c7ea3b920f51669fa8184ee
addoun abdellah <addoun.abdellah@gmail.com>
2019-12-10 15:10:31 +0100

    Creating strength slider

--- lib/screens/home/settings-form.dart
+++ lib/screens/home/settings-form.dart
@@ -46,6 +46,21 @@ class _SettingsFormState extends State<SettingsForm> {
             onChanged: (value) => setState(() => _currentSugar = value),
             decoration: textInputDecoration,
           ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Slider(
+            value: (_currentStrength ?? 100).toDouble(),
+            activeColor: Colors.brown[_currentStrength ?? 100],
+            inactiveColor: Colors.brown[_currentStrength ?? 100],
+            min: 100,
+            max: 900,
+            divisions: 8,
+            onChanged: (value) => setState(() => _currentStrength = value.round()),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
           FlatButton(
             color: Colors.brown[700],
             child: Text(
