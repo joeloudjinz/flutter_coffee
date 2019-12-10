8f023b1d3da7f93eba3de1027477384ec43abce7
addoun abdellah <addoun.abdellah@gmail.com>
2019-12-10 14:58:49 +0100

    Creating name & sugars form fields

---
+++ lib/screens/home/settings-form.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:brew_crew/shared/constants.dart';
+
+class SettingsForm extends StatefulWidget {
+  @override
+  _SettingsFormState createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+  final _formkey = GlobalKey<FormState>();
+  final List<String> sugars = ['0', '1', '2', '3', '4'];
+  String _currentName;
+  String _currentSugar;
+  int _currentStrength;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formkey,
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Update your brew settings',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          TextFormField(
+            decoration: textInputDecoration.copyWith(hintText: 'Name'),
+            validator: (value) => value.isEmpty ? 'Name is required' : null,
+            onChanged: (value) => setState(() => _currentName = value),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          DropdownButtonFormField(
+            value: _currentSugar ?? '0',
+            items: sugars
+                .map(
+                  (s) => DropdownMenuItem(
+                    value: s,
+                    child: Text('$s sugars'),
+                  ),
+                )
+                .toList(),
+            onChanged: (value) => setState(() => _currentSugar = value),
+            decoration: textInputDecoration,
+          ),
+          FlatButton(
+            color: Colors.brown[700],
+            child: Text(
+              'Update',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
