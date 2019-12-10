217820747790ea07d8858bbc0faa8a8cf5c1f94c
addoun abdellah <addoun.abdellah@gmail.com>
2019-12-10 09:26:02 +0100

    Implementing sign in logic

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -26,6 +26,19 @@ class AuthService {
   }
 
   // sign in with email & password
+  Future signIn(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return _createUser(result.user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
   // register with email & password
   Future register(String email, String password) async {
     try {
